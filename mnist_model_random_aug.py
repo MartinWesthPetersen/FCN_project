7edb36a6a7c8a049dbd7e5af141f593aa367b038
Martin Westh Petersen <martin_westh@hotmail.com>
2018-06-22 14:00:09 +0200

    Random augmentation model

---
+++ mnist_model_random_aug.py
@@ -0,0 +1,104 @@
+# IMPORTS
+import numpy as np
+import pickle
+import unet_classification as unet
+import pickle
+import os
+
+# LOAD TRAIN (AND TEST?) DATA AND LABELS
+train_path = '../mnistSMALL/mnist100_train.p'
+val_path = '../mnistSMALL/mnist100_val.p'
+results_path = 'results/model_performance_val100.p'
+
+# hacky solution to hardcode the amount of images per class in the training set
+nr_real_examples = 100
+nr_artificial_examples = 200
+weights = [[1.0]*nr_real_examples + [0.5]*nr_artificial_examples for digit in range(10)]
+weights = np.array(weights).flatten()
+
+# data has structure: train_dict[digit] = 100 real + 200 artifcial images, stacked
+with open(train_path, 'rb') as handle:
+    train_dict = pickle.load(handle)
+
+# Artificial train images
+artificial = {}
+for digit in range(10):
+    artificial[digit] = np.load("mnist_random_artificial\\new_images_" 
+              + str(digit) + ".npy")
+
+# Shape is now (28, 28, 1000)
+train_tensor = np.dstack([np.dstack((train_dict[digit], artificial[digit])) for digit in train_dict.keys()])
+train_tensor = np.moveaxis(train_tensor,  -1, 0)
+# Shape is now (1000, 28, 28, 1)
+train_tensor = np.expand_dims(train_tensor, axis=3)
+
+# make labels
+train_labels = []
+for digit in train_dict.keys():
+    for _ in range(nr_real_examples + nr_artificial_examples):
+        train_labels.append(digit)
+
+train_labels = np.array(train_labels)
+
+with open(val_path, 'rb') as handle:
+    val_dict = pickle.load(handle)
+val_tensor = np.dstack([val_dict[digit] for digit in val_dict.keys()])
+val_tensor = np.moveaxis(val_tensor,  -1, 0)
+val_tensor = np.expand_dims(val_tensor, axis=3)
+val_labels = []
+for digit in val_dict.keys():
+    for _ in range(val_dict[0].shape[-1]):
+        val_labels.append(digit)
+val_labels = np.array(val_labels)
+
+num_classes = 10
+conv_ksize = 5
+num_channels_base = 16  # (or 8 for starters?)
+scale_depth = 2
+learning_rate = 1e-5
+bn_decay = 0.9
+model_dir_name = "MNIST_model_random_aug"
+
+batch_size = 16
+training_steps = 100
+log_interval = 50
+
+rounds = 500
+classifier = unet.create_unet_model(train_tensor, num_classes, conv_ksize,
+                                    num_channels_base, scale_depth,
+                                    learning_rate, bn_decay, model_dir_name)
+
+# load performance dictionary to continue training, otherwise initialize new
+if os.path.isfile(results_path):
+    with open(results_path, 'rb') as handle:
+        model_perf = pickle.load(handle)
+
+model_perf = {'train': [], 'val': []}
+
+for j in range(rounds):
+    print("***** TRAINING MODEL " + str(j) + " *****")
+    classifier = unet.train_unet_model(train_tensor, train_labels, classifier, batch_size,
+                                       training_steps, log_interval, weights)
+    print("***** EVALUATING MODEL " + str(j) + " *****")
+    # Evaluate on training
+    train_pred = unet.predict_unet_model(train_tensor, classifier)
+    train_acc = unet.compute_accuracy(train_labels, train_pred.flatten())
+    model_perf['train'].append(train_acc)
+
+    # Evaluate on validation
+    val_pred = unet.predict_unet_model(val_tensor, classifier)
+    val_acc = unet.compute_accuracy(val_labels, val_pred.flatten())
+    model_perf['val'].append(val_acc)
+
+    with open(results_path, 'wb') as handle:
+        pickle.dump(model_perf, handle)
+
+    print('\n \n ***** RESULTS *****:', model_perf)
+
+    # Early stopping criterion
+    if j >= 50:
+        current_val_error = model_perf['val'][j]
+        if (model_perf['val'][j-1] >= current_val_error and model_perf['val'][j-2] >= current_val_error
+        and model_perf['val'][j-3] >= current_val_error):
+            print('EARLY STOPPING AFTER %s EPROCHS.' %j)
+            break
